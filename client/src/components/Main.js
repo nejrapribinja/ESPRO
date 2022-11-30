b14ed196ca7d6a436e794b362c3f0c94ddaf5d7e
Nejra <nejra.pribinja@gmail.com>
2022-11-30 16:44:09 +0100

    first commit

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { Nav, NavLink, Col, Row, Navbar, Container, NavDropdown, Button } from "react-bootstrap";
-import two from "../images/2.png";
-import three from "../images/3.png";
-import four from "../images/4.png";
-import { IoIosSearch } from "react-icons/io";
-import { HiOutlineUser } from "react-icons/hi";
 import { BsBag } from "react-icons/bs";
+import { BiUser, BiSearch } from "react-icons/bi";
+import { AiOutlineArrowDown } from "react-icons/ai";
+import logo from "../images/ESPRO Logo.png";
 
 const Main = () => {
   return (
@@ -16,22 +14,6 @@ const Main = () => {
         </Col>
       </Row>
       <div className="homep">
-        {/* <Row className="nv ">
-          <Col md={4} className="d-flex ">
-            <h5>Shop</h5>
-            <h5>Our story</h5>
-          </Col>
-          <Col md={4} className="d-flex justify-content-center align-items-center text-center">
-            <h5>ESPRO</h5>
-          </Col>
-          <Col md={4} className="d-flex justify-content-end align-items-center text-center">
-            <h5>Brew guides</h5>
-            <h5>Blog</h5>
-            <IoIosSearch className="icon" />
-            <HiOutlineUser className="icon" />
-            <BsBag className="icon" />
-          </Col>
-        </Row> */}
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
           <Container>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -43,13 +25,16 @@ const Main = () => {
                 </Nav>
               </Col>
               <Col className="text-center">
-                <Navbar.Brand href="#pocetnaStrana">ESPRO</Navbar.Brand>
+                <Navbar.Brand href="#pocetnaStrana">
+                  <img src={logo} />
+                </Navbar.Brand>
               </Col>
               <Col>
                 <Nav className="d-flex justify-content-end align-items-center text-end">
                   <Nav.Link href="#kontakt">Brew guides</Nav.Link>
-                  <Nav.Link href="/login">Blog</Nav.Link> <IoIosSearch className="icon" />
-                  <HiOutlineUser className="icon" />
+                  <Nav.Link href="/login">Blog</Nav.Link>
+                  <BiSearch className="icon" />
+                  <BiUser className="icon" />
                   <BsBag className="icon" />
                 </Nav>
               </Col>
@@ -58,20 +43,22 @@ const Main = () => {
         </Navbar>
         <Row
           className="d-flex justify-content-center align-items-center text-center"
-          style={{ height: "77vh" }}>
-          <Col md={6} className=" ">
-            <h1>BREW ANYWHERE. BE ANYWHERE.</h1>
-            <h4>
+          style={{ height: "80vh" }}>
+          <Col md={7}>
+            <h1 className="hd1">Brew anywhere. Be anywhere.</h1>
+            <p className="t1">
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-              invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
-              accusam et justo duo.
-            </h4>
+              invidunt ut
+              <br /> labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+              et justo duo.
+            </p>
             <Button>POUR ON THE GO</Button>
           </Col>
         </Row>
-        <Row className="d-flex justify-content-center align-items-end text-center">
-          <Col md={3}>
-            <h4>explore</h4>
+        <Row className="d-flex justify-content-center align-items-center text-center">
+          <Col md={3} className="d-flex justify-content-center align-items-center">
+            <p className="t2">explore</p>
+            <AiOutlineArrowDown style={{ color: "white", fontSize: "18px" }} />
           </Col>
         </Row>
       </div>
