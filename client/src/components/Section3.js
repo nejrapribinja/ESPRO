b342542e8dddcec49684d24fa2ebac2b884fce3f
Nejra <nejra.pribinja@gmail.com>
2022-12-02 18:21:58 +0100

    first commit

--- client/src/components/Section3.js
+++ client/src/components/Section3.js
@@ -10,16 +10,20 @@ const Section3 = () => {
           <p className="t6">Explore our products</p>
         </Col>
       </Row>
-      <Row className="d-flex justify-content-center align-items-center text-center">
-        <Col md={12} className="d-flex justify-content-center align-items-center text-center">
-          <Product />
-          <Product />
-          <Product />
+      <Row className="d-flex justify-content-evenly align-items-center text-center">
+        <Col md={3} className="text-start sc31 t15">
+          <p>French press</p>
+        </Col>
+        <Col md={3} className="text-start sc32 t15">
+          <p>Pour over</p>
+        </Col>
+        <Col md={3} className="text-start sc33 t15">
+          <p>Cold brew</p>
         </Col>
       </Row>
       <Row className="d-flex justify-content-center align-items-center text-center">
         <Col md={5}>
-          <Button className="btn1">Shop all products</Button>
+          <Button className="btn1 mt-4">Shop all products</Button>
         </Col>
       </Row>
     </Container>
--- client/src/components/Section7.js
+++ client/src/components/Section7.js
@@ -1,27 +1,36 @@
 import React, { useState, useEffect } from "react";
 import { Nav, NavLink, Col, Row, Navbar, Container, Table, Button } from "react-bootstrap";
-import Product from "./Product";
+import ten from "../images/10.png";
 
 const Section7 = () => {
   return (
     <Container fluid style={{ paddingTop: "4rem", paddingBottom: "4rem" }} className="section7">
-      <Row className="d-flex justify-content-center align-items-center text-center">
-        <Col md={5}>
-          <p className="t6">Explore our products</p>
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-center align-items-center text-center">
-        <Col md={12} className="d-flex justify-content-center align-items-center text-center">
-          <Product />
-          <Product />
-          <Product />
-        </Col>
-      </Row>
-      <Row className="d-flex justify-content-center align-items-center text-center">
-        <Col md={5}>
-          <Button className="btn1">Shop all products</Button>
-        </Col>
-      </Row>
+      <Container>
+        <Row className="d-flex justify-content-center align-items-center text-center">
+          <Col md={6} className="d-flex justify-content-start align-items-center text-start">
+            <div>
+              <p className="t5">The ultimate brew guides</p>
+              <p className="t14">COFFEE TO WATER RATIOS: HOW TO MEASURE COFFEE LIKE A PRO</p>
+              <p>
+                <b>
+                  You can taste it when you’ve brewed with the perfect coffee to water
+                  <br /> ratio.
+                </b>
+                The acidity pops every so slightly, enhancing the rich flavors and
+                <br /> aromas. Your coffee’s mouthfeel is smooth and satisfying. And the flavor
+                <br /> is just strong enough that you can sense all its nuances across your
+                <br /> 10,000 taste buds, but not so strong that it’s uncomfortable.
+                <br />
+                <br /> It’s an incredible experience—and too often, a hard one to come by.
+              </p>
+              <Button className="btn3 mt-4">Learn more</Button>
+            </div>
+          </Col>
+          <Col md={6} className=" sc7">
+            <img src={ten} width="620" height="450" />
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
